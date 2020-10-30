e25c87662df00bd38740b809af3543eb0da01471
Junoh Moon <mjo970625@gmail.com>
2020-10-30 20:26:31 +0900

    Add comments for better understanding

--- testlock.c
+++ testlock.c
@@ -11,19 +11,24 @@ int main() {
 		pid = fork();
 		if(pid) {
 			printf(1, "process %d is created\n", i);
-			sleep(100);
+			sleep(10);
 		}
 		else
 			break;
 	}
 
 	if (pid) {
-		sleep(1000);
+		sleep(100);
 		testlock();
 		for(i = 0; i < 10; ++i) {
 			wait();
 		}
 	} else {
+		// When testlock is called, it first checks whether it is holding a lock.
+		// Since child's pid is different from the parent, the chlid process is not holding the lock.
+		// Thus, the child tries to acquire the lock, but it cannot 
+		// due to the fact that the same lock is actually held by the parent.
+		// Consequently, what children can do is just go to sleep and wait for the parent's wakeup
 		testlock();
 		printf(1, "%d have acquired lock\n", i);
 		testlock();
