4be4c509d6c78d67c120df0a253c5deb5cbab4a6
Junoh Moon <mjo970625@gmail.com>
2020-11-04 18:52:11 +0900

    Add semaphore feature
    To be honest, semaphore is nothing but an extension of sleeplock, so I
    brought most of the logic from sleeplock.c.

--- semaphore.h
+++ semaphore.h
@@ -1,6 +1,11 @@
 #pragma once
+#include "types.h"
+#include "spinlock.h"
 
 struct semaphore {
+  uint availableSeats;       // Is the lock held?
+  struct spinlock lk; // spinlock protecting this sleep lock
+  struct proc* head;
 };
 
 void initsema(struct semaphore* lk, int count);
